f5bebc82174314ca4817ea93fcc5a441d63628a5
Jaeyeonling <jaeyeonling@gmail.com>
2019-11-07 01:58:37 +0900

    test: pass test

--- src/test/java/com/depromeet/warmup/ApplicationTest.java
+++ src/test/java/com/depromeet/warmup/ApplicationTest.java
@@ -1,13 +1,12 @@
 package com.depromeet.warmup;
 
+import com.depromeet.warmup.support.BaseSupports;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ExtendWith(SpringExtension.class)
-@SpringBootTest
-class ApplicationTest {
+class ApplicationTest extends BaseSupports {
 
     @Test
     void contextLoads() { }
