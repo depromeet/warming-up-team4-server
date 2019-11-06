0d5ec145c6f56790696083af8789e10c6af91114
Jaeyeonling <jaeyeonling@gmail.com>
2019-11-07 01:51:48 +0900

    refactor: 사이드이팩트 없는 순수 함수형으로 수정

--- src/main/java/com/depromeet/warmup/domain/item/Item.java
+++ src/main/java/com/depromeet/warmup/domain/item/Item.java
@@ -10,7 +10,13 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -33,8 +39,6 @@ public class Item extends DateAuditEntity implements ProtobufConverter<ItemOuter
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private BarterStatus barterStatus;
-
-    // TODO: User
 
     @ManyToOne
     private User user;
